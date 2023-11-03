cdcf1ad6156c5b3627260b096de1f7af1f00cd1b
CMOISDEAD <camilodavina@gmail.com>
2023-11-02 20:35:58 -0500

    feat: html embed md and tailwind css support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import * as path from "node:path";
 import { compileComplements, getMd, writeHtml } from "./handler.ts";
 import { parseMd } from "./parser.ts";
 import chokidar from "chokidar";
 import minimist from "minimist";
+import liveServer from "live-server";
 
-const { f } = minimist(Bun.argv);
+const { f, l } = minimist(Bun.argv);
 
 const main = async () => {
   if (!f) return console.log("Please specify a file with -f");
@@ -19,3 +21,10 @@ chokidar.watch(f).on("all", () => {
   console.log("File changed");
   main();
 });
+
+if (l)
+  liveServer.start({
+    port: 8080,
+    root: path.join(import.meta.dir, "../dist"),
+    open: false,
+  });
--- src/parser.ts
+++ src/parser.ts
@@ -10,15 +10,19 @@ import rehypePrettyCode from "rehype-pretty-code";
 // custom plugins
 import remarkWrapSections from "./plugins/remark.ts";
 import rehypeWrapSections from "./plugins/rehype.ts";
+import remarkFrontmatter from "remark-frontmatter";
+import rehypeRaw from "rehype-raw";
 
 // parse markdown to html
 export const parseMd = async (content: any) => {
   const file = await unified()
     .use(remarkParse)
+    .use(remarkFrontmatter, ["yaml"])
     .use(remarkGfm)
     .use(remarkMath)
     .use(remarkWrapSections)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypePrettyCode, {
       theme: "poimandres",
     })
